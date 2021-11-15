31ca02a5021bfe9e435eeb022a141ac6dfcecb0b
Arnaud Hatzenbuhler <arnaud.hatzenbuhler@gmail.com>
2021-11-15 14:16:32 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,14 @@
 #!/bin/env bash
 
+sudo apt-get update
 sudo apt-get install build-essential procps curl file git -y
+
+git config user.email arnaud.hatzenbuhler@gmail.com
+git config user.name Arnaud Hatzenbuhler
 
 # Install brew
 export NONINTERACTIVE=1
+export LANG="en_US.UTF-8"
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> ~/.bash_profile
 echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> ~/.bashrc
@@ -14,7 +19,16 @@ while read package
 do
     brew install $package
 done < packages.txt
+echo 'export PATH="/home/linuxbrew/.linuxbrew/opt/python@3.10/bin:$PATH"' >> ~/.bashrc
 brew cleanup --prune=all
+
+# Install Docker Engine (it's broken on brew)
+sudo apt-get update
+sudo apt-get install ca-certificates curl gnupg lsb-release
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io
 
 # Install VSCode (it's broken on brew)
 wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
